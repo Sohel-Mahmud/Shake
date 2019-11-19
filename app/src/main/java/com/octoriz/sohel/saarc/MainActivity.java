ed37c4fbba7a55614b6bdf0675c5fcde8dcdf030
Sohel Mahmud <sohel7avon@gmail.com>
2019-11-20 01:04:41 +0600

    fixing shaking

--- app/src/main/java/com/octoriz/sohel/saarc/MainActivity.java
+++ app/src/main/java/com/octoriz/sohel/saarc/MainActivity.java
@@ -132,5 +132,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ShakeActivity.class));
+            }
+        });
     }
 }
