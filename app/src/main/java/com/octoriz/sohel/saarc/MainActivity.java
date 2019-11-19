15897bdbd11ea856075f2e22a3cd7d2334f66332
Sohel Mahmud <sohel7avon@gmail.com>
2019-11-19 23:31:37 +0600

    layout created

--- app/src/main/java/com/octoriz/sohel/saarc/MainActivity.java
+++ app/src/main/java/com/octoriz/sohel/saarc/MainActivity.java
@@ -1,14 +1,38 @@
 package com.octoriz.sohel.saarc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.octoriz.sohel.saarc.Entity.Country;
+import com.octoriz.sohel.saarc.ViewModel.ShakeViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ShakeViewModel shakeViewModel;
+    private TextView txtCountryList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtCountryList = findViewById(R.id.countryList);
+
+        shakeViewModel = ViewModelProviders.of(this).get(ShakeViewModel.class);
+
+        shakeViewModel.getCountryList().observe(this, new Observer<List<Country>>() {
+            @Override
+            public void onChanged(List<Country> countries) {
+                for(int i=0; i<countries.size(); i++){
+                    txtCountryList.append(countries.get(i).getCountry_id()+countries.get(i).getCountryName()+"\n");
+
+                }
+            }
+        });
     }
 }
